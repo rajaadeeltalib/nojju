b06360e13f0b6971fb837869d67427969356a5a0
rajaadeeltalib <adeeltalib6270@gmail.com>
2023-06-21 10:55:00 +0500

    code updated

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,35 +1,60 @@
-import { MetadataRoute } from 'next'
- 
-export default function sitemap(): MetadataRoute.Sitemap {
-  return [
-    {
-      url: 'https://www.nojju.com/',
-      lastModified: new Date(),
-    },
-    {
-      url: 'https://www.nojju.com/blog',
-      lastModified: new Date(),
-    },
-    {
-      url: 'https://www.nojju.com/about',
-      lastModified: new Date(),
-    },
-    {
-        url: 'https://www.nojju.com/contact',
-        lastModified: new Date(),
-      },
-      {
-        url: 'https://www.nojju.com/terms',
-        lastModified: new Date(),
-      },
-      {
-        url: 'https://www.nojju.com/privacy',
-        lastModified: new Date(),
-      },
-      {
-        url: 'https://www.nojju.com/disclaimer',
-        lastModified: new Date(),
-      },
+import { client } from "../lib/sanityClient";
+import { urlForImage } from "../../sanity/lib/image";
+
+const getAllBlogsPosts = async () => {
+    const res = await client.fetch(`*[_type=='posts'] {slug} | order(date desc) `);
+  
+    return res;
+  };
+
+export default async function sitemap() {
+    const allBlogsPosts = await getAllBlogsPosts();
+    
+const baseUrl = "https://www.nojju.com/";
+const blogUrl = "https://www.nojju.com/blog/";
+
+
+
+    const data = allBlogsPosts.map((post:any, index:number) => {
       
-  ]
+        return {
+            url:`${blogUrl}${allBlogsPosts[index].slug.current}`,
+            lastModified: new Date(),
+    };
+}) ?? [];
+    return [
+       
+        {
+            url: baseUrl,
+            lastModified: new Date(),
+        },
+        {
+            url: `${baseUrl}blog`,
+            lastModified: new Date(),
+        },
+        {
+            url: `${baseUrl}about`,
+            lastModified: new Date(),
+        },
+        {
+            url: `${baseUrl}contact`,
+            lastModified: new Date(),
+        },
+        {
+            url: `${baseUrl}terms`,
+            lastModified: new Date(),
+        },
+        {
+            url: `${baseUrl}privacy`,
+            lastModified: new Date(),
+        },
+        {
+            url: `${baseUrl}disclaimer`,
+            lastModified: new Date(),
+        },
+
+        ...data,
+        
+    
+    ]
 }
