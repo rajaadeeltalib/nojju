b94efacfa5e1751cf19e71137f88d7b25c4f464e
rajaadeeltalib <adeeltalib6270@gmail.com>
2023-06-22 14:53:51 +0500

    code updated

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,7 +12,7 @@ export const metadata:Metadata = {
     default: 'Nojju',
     template: '%s | Nojju',
   },
-  description: 'Unleashing Outdoor Camping Adventures and Products Insights',
+  description: 'Discover the best camping gear for your outdoor adventures. From camping tents and sleeping bags to portable stoves and lanterns, our blog provides expert reviews, recommendations, and product guides.',
   keywords: 'camping, Camping Tent, Sleeping bag, Camping stove, Camping chairs, Lantern/Flashlight, Cooler/Ice chest, Camping cookware set, Campfire grill, Hammock, Camping mattress/pad, Backpack, Water filter/purifier, Portable camping shower, Camping table, Campfire cooking utensils, Inflatable camping pillow, Portable camping toilet, First aid kit, Camping knife/multi-tool, Portable power bank/solar charger,',
   verification: {
     google:"czrSo0Na4yV7AyXssppMJRQ0-jvBUCyG03LE8IgkEqk",
