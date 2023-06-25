36f1b8ed94544000cc5f1fc0fa5ceb8f48fdb027
rajaadeeltalib <114896091+rajaadeeltalib@users.noreply.github.com>
2023-06-26 01:01:07 +0500

    code updated

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,7 +12,7 @@ export const metadata:Metadata = {
     default: 'Nojju | Camping & Hiking Gear',
     template: '%s | Nojju',
   },
-  description: 'Discover the best Camping and Hiking Gear for your outdoor adventures.',
+  description: 'Discover the best Camping and Hiking Gear for your outdoor adventures',
   keywords: 'camping gear, Hiking Gear, Camping Tent, Sleeping bag, Camping stove, Camping chairs, Lantern/Flashlight, Cooler/Ice chest, Camping cookware set, Campfire grill, Hammock, Camping mattress/pad, Backpack, Water filter/purifier, Portable camping shower, Camping table, Campfire cooking utensils, Inflatable camping pillow, Portable camping toilet, First aid kit, Camping knife, multi-tool, Portable power bank/solar charger,',
   verification: {
     google:"czrSo0Na4yV7AyXssppMJRQ0-jvBUCyG03LE8IgkEqk",
